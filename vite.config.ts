aee6535cf67a48dcf5deeb2c858b78ad22e9f2fd
chocosd <hello@stevendix.co.uk>
2025-04-11 10:17:48 +0100

    core (setup): fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,24 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
+import { dirname, resolve } from "path";
+import { fileURLToPath } from "url";
 import { defineConfig } from "vite";
 
-// https://vite.dev/config/
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@components": resolve(__dirname, "src/components"),
+      "@ui": resolve(__dirname, "src/components/ui"),
+      "@layouts": resolve(__dirname, "src/layouts"),
+      "@pages": resolve(__dirname, "src/pages"),
+      "@hooks": resolve(__dirname, "src/hooks"),
+      "@lib": resolve(__dirname, "src/lib"),
+      "@types": resolve(__dirname, "src/types"),
+      "@router": resolve(__dirname, "src/router"),
+    },
+  },
 });
